9fe0bf742cefd99c743e3dd5b06565dd449b6bb9
ykawato <ykawato@gmail.com>
2013-12-20 12:39:37 +0900

    bootstrap grid, title with breadcrumb
    uses Breadcrumb Trail plugin

--- archive.php
+++ archive.php
@@ -1,113 +1,137 @@
 <?php get_header(); ?>
 
 
+<!--=== Title & Breadcrumb ===-->
+    <div class="container">
+
+            <?php if (is_category()) { ?>
+                <h1 class="archive-title h2">
+                    <span><?php _e( 'Posts Categorized:', 'bonestheme' ); ?></span> <?php single_cat_title(); ?>
+                </h1>
+
+            <?php } elseif (is_tag()) { ?>
+                <h1 class="archive-title h2">
+                    <span><?php _e( 'Posts Tagged:', 'bonestheme' ); ?></span> <?php single_tag_title(); ?>
+                </h1>
+
+            <?php } elseif (is_tax()) { ?>
+                    <h1 class="archive-title h2">
+                            <span><?php _e( 'Posts Tagged:', 'bonestheme' ); ?></span> <?php single_tag_title(); ?>
+                    </h1>
+
+            <?php } elseif (is_author()) {
+                global $post;
+                $author_id = $post->post_author;
+            ?>
+                <h1 class="archive-title h2">
+
+                    <span><?php _e( 'Posts By:', 'bonestheme' ); ?></span> <?php the_author_meta('display_name', $author_id); ?>
+
+                </h1>
+            <?php } elseif (is_day()) { ?>
+                <h1 class="archive-title h2">
+                    <span><?php _e( 'Daily Archives:', 'bonestheme' ); ?></span> <?php the_time('l, F j, Y'); ?>
+                </h1>
+
+            <?php } elseif (is_month()) { ?>
+                    <h1 class="archive-title h2">
+                        <span><?php _e( 'Monthly Archives:', 'bonestheme' ); ?></span> <?php the_time('F Y'); ?>
+                    </h1>
+
+            <?php } elseif (is_year()) { ?>
+                    <h1 class="archive-title h2">
+                        <span><?php _e( 'Yearly Archives:', 'bonestheme' ); ?></span> <?php the_time('Y'); ?>
+                    </h1>
+            <?php } ?>
+
+
+        <div class="pull-right breadcrumb">
+            <?php if ( function_exists( 'breadcrumb_trail' ) ) { breadcrumb_trail(); } ?>
+        </div>
+    </div><!--/container-->
+<!--=== End Title & Breadcrumb ===-->
+
+
+
+
+
+
+
       <div class="row ">
 
-        <div class="col-xs-12 col-sm-9">
+        <div class="col-lg-9 col-md-9 col-xs-12 col-sm-9">
 
 
-							<?php if (is_category()) { ?>
-								<h1 class="archive-title h2">
-									<span><?php _e( 'Posts Categorized:', 'bonestheme' ); ?></span> <?php single_cat_title(); ?>
-								</h1>
-
-							<?php } elseif (is_tag()) { ?>
-								<h1 class="archive-title h2">
-									<span><?php _e( 'Posts Tagged:', 'bonestheme' ); ?></span> <?php single_tag_title(); ?>
-								</h1>
-
-							<?php } elseif (is_tax()) { ?>
-								<h1 class="archive-title h2">
-									<span><?php _e( 'Posts Tagged:', 'bonestheme' ); ?></span> <?php single_tag_title(); ?>
-								</h1>
-
-							<?php } elseif (is_author()) {
-								global $post;
-								$author_id = $post->post_author;
-							?>
-								<h1 class="archive-title h2">
-
-									<span><?php _e( 'Posts By:', 'bonestheme' ); ?></span> <?php the_author_meta('display_name', $author_id); ?>
-
-								</h1>
-							<?php } elseif (is_day()) { ?>
-								<h1 class="archive-title h2">
-									<span><?php _e( 'Daily Archives:', 'bonestheme' ); ?></span> <?php the_time('l, F j, Y'); ?>
-								</h1>
-
-							<?php } elseif (is_month()) { ?>
-									<h1 class="archive-title h2">
-										<span><?php _e( 'Monthly Archives:', 'bonestheme' ); ?></span> <?php the_time('F Y'); ?>
-									</h1>
-
-							<?php } elseif (is_year()) { ?>
-									<h1 class="archive-title h2">
-										<span><?php _e( 'Yearly Archives:', 'bonestheme' ); ?></span> <?php the_time('Y'); ?>
-									</h1>
-                            <?php } elseif (is_post_type_archive()) { ?>
-                                    <h1 class="archive-title h2">
-                                        <span><?php _e("Posts In:", "bonestheme"); ?></span> <?php post_type_archive_title(); ?>
-                                    </h1>
-                            <?php } elseif (is_tax()) { ?>
-                                    <h1 class="archive-title h2">
-                                        <span><?php _e("Posts Categorized:", "bonestheme"); ?></span> <?php single_term_title(); ?>
-                                    </h1>
-                            <?php } ?>
 
 
-							<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+            <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
-							<article id="post-<?php the_ID(); ?>" <?php post_class( 'clearfix' ); ?> role="article">
+            <article id="post-<?php the_ID(); ?>" <?php post_class( 'clearfix' ); ?> role="article">
 
-								<header class="article-header">
+                <header class="article-header">
 
-									<h3 class="h2"><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h3>
-<?php /*
-									<p class="byline vcard"><?php
-										printf(__( 'Posted <time class="updated" datetime="%1$s" pubdate>%2$s</time> by <span class="author">%3$s</span> <span class="amp">&</span> filed under %4$s.', 'bonestheme' ), get_the_time('Y-m-j'), get_the_time(__( 'F jS, Y', 'bonestheme' )), bones_get_the_author_posts_link(), get_the_category_list(', '));
-									?></p>
-*/ ?>
-								</header> <!-- end article header -->
+                    <h3 class="h2"><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h3>
+                    <p class="byline vcard"><?php
+                        printf(__( 'Posted <time class="updated" datetime="%1$s" pubdate>%2$s</time> by <span class="author">%3$s</span> <span class="amp">&</span> filed under %4$s.', 'bonestheme' ), get_the_time('Y-m-j'), get_the_time(__( 'F jS, Y', 'bonestheme' )), bones_get_the_author_posts_link(), get_the_category_list(', '));
+                    ?></p>
 
-								<section class="entry-content clearfix">
+                </header> <!-- end article header -->
 
-									<?php the_post_thumbnail( 'bones-thumb-300' ); ?>
+                <section class="entry-content clearfix">
 
-									<?php the_excerpt(); ?>
+                    <?php the_post_thumbnail( 'bones-thumb-300' ); ?>
 
-								</section> <!-- end article section -->
+                    <?php the_excerpt(); ?>
 
-								<footer class="article-footer">
+                </section> <!-- end article section -->
 
-								</footer> <!-- end article footer -->
+                <footer class="article-footer">
 
-							</article> <!-- end article -->
+                </footer> <!-- end article footer -->
 
-							<?php endwhile; ?>
+            </article> <!-- end article -->
 
-                                <?php bootstrap_pagination(); ?>
+            <?php endwhile; ?>
 
-							<?php else : ?>
+                    <?php if ( function_exists( 'bones_page_navi' ) ) { ?>
+                        <?php bones_page_navi(); ?>
+                    <?php } else { ?>
+                        <nav class="wp-prev-next">
+                            <ul class="clearfix">
+                                <li class="prev-link"><?php next_posts_link( __( '&laquo; Older Entries', 'bonestheme' )) ?></li>
+                                <li class="next-link"><?php previous_posts_link( __( 'Newer Entries &raquo;', 'bonestheme' )) ?></li>
+                            </ul>
+                        </nav>
+                    <?php } ?>
 
-									<article id="post-not-found" class="hentry clearfix">
-										<header class="article-header">
-											<h1><?php _e( 'Oops, Post Not Found!', 'bonestheme' ); ?></h1>
-										</header>
-										<section class="entry-content">
-											<p><?php _e( 'Uh Oh. Something is missing. Try double checking things.', 'bonestheme' ); ?></p>
-										</section>
-										<footer class="article-footer">
-												<p><?php _e( 'This is the error message in the archive.php template.', 'bonestheme' ); ?></p>
-										</footer>
-									</article>
+            <?php else : ?>
 
-							<?php endif; ?>
+                    <article id="post-not-found" class="hentry clearfix">
+                        <header class="article-header">
+                            <h1><?php _e( 'Oops, Post Not Found!', 'bonestheme' ); ?></h1>
+                        </header>
+                        <section class="entry-content">
+                            <p><?php _e( 'Uh Oh. Something is missing. Try double checking things.', 'bonestheme' ); ?></p>
+                        </section>
+                        <footer class="article-footer">
+                                <p><?php _e( 'This is the error message in the archive.php template.', 'bonestheme' ); ?></p>
+                        </footer>
+                    </article>
+
+            <?php endif; ?>
+
 
 
         </div><!--/span-->
-						<?php get_sidebar(); ?>
+
+        <!-- sidebar -->
+        
+        
+<?php get_sidebar(); ?>
+
+      </div><!--/row-->
 
 
-    </div><!--/row-->
+
 
 <?php get_footer(); ?>
